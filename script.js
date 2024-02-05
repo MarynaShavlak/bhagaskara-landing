4c33a2cdfc51234c59e16f8c08be22168b535089
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-02-05 20:35:06 +0200

    add start owl carousel code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel({
     loop: true,
-    // margin: 10,
+    navText: `<span>button</span>`,
     nav: true,
     responsive: {
       375: {
